9a77d946abfbd5101b5063b5c135adec3db2f17f
leandrosfreitas <leandrosanfreitas@gmail.com>
2025-02-14 20:41:40 -0300

    finalizando desafio amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById('amigo');
+    let nome = input.value.trim();
+
+    if (nome) {
+        listaAmigos.push(nome);
+        document.getElementById('resultado').innerHTML += nome + "<br>";
+        input.value = "";
+    } else {
+        alert('Digite um nome válido.');
+    }
+}
+
+function sortearAmigo() {
+    if (listaAmigos.length) {
+        let nomeSorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)]
+        document.getElementById('resultado').innerHTML = nomeSorteado
+    } else {
+        alert('Lista está vazia, adicione amigos para sortear.')
+    }
+}
